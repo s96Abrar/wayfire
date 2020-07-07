6f9365b2ab4ca21e7bcc99ffa780c532e28a3e34
Scott Moreau <oreaus@gmail.com>
2020-07-07 16:19:59 -0600

    vswitch: Switch workspace after moving view
    This fixes switching workspaces with a view, while scale is active.

--- plugins/single_plugins/vswitch.cpp
+++ plugins/single_plugins/vswitch.cpp
@@ -293,7 +293,6 @@ class vswitch : public wf::plugin_interface_t
         cws.y += animation.dy.end;
 
         auto output_g = output->get_relative_geometry();
-        output->workspace->set_workspace(cws);
 
         if (grabbed_view)
         {
@@ -311,6 +310,7 @@ class vswitch : public wf::plugin_interface_t
             data.to   = output->workspace->get_current_workspace();
             output->emit_signal("view-change-viewport", &data);
         }
+        output->workspace->set_workspace(cws);
     }
 
     void stop_switch()
